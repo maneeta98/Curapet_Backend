9410117fff54e74fdbfb7d89018ff9287269dd36
maneeta98 <manitapariyar2005@gmail.com>
2025-06-13 21:22:31 +0545

    added authControllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
-// const User = require('../models/User');
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 // exports.register = async (req, res) => {
 //   try {

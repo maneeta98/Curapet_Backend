72cbb06ecd910f7730d283497d32950886641119
maneeta98 <manitapariyar2005@gmail.com>
2025-06-13 21:26:39 +0545

    added Jwt Token

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,9 +7,9 @@ const authMiddleware = (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-    //   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    //   req.user = decoded;
-    //   next();
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
     } catch (err) {
       return res.status(401).json({ message: 'Invalid token' });
     }

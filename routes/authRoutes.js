b5e7990b164d5514021361fb1660cfcfbabe110b
maneeta98 <manitapariyar2005@gmail.com>
2025-06-13 21:28:29 +0545

    added authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,17 +1,17 @@
-// const express = require('express');
-// const { register, login } = require('../controllers/authController');
-// const authMiddleware = require('../middlewares/authMiddleware');
+const express = require('express');
+const { register, login } = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 
 
-// const router = express.Router();
+const router = express.Router();
 
 // router.post('/register', register);
 // router.post('/login', login);
 
-// // Optional protected test route
+// Optional protected test route
 // router.get('/dashboard', authMiddleware, (req, res) => {
 //   res.json({ message: `Welcome, user ${req.user.id}` });
 // });
 
-// module.exports = router;
+module.exports = router;

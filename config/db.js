60b371b7d4855d45606d7f350f5e136cd210d3b1
maneeta98 <manitapariyar2005@gmail.com>
2025-06-13 21:21:09 +0545

    connect to the mongodb database

--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
-// const mongoose = require("mongoose")
+const mongoose = require("mongoose")
 
-// const CONNECTION_STRING = process.env.MONGODB_URI
+const CONNECTION_STRING = process.env.MONGODB_URI
 
-// const connectDB = async () => {
-//     try{
-//         await mongoose.connect(
-//             CONNECTION_STRING, 
-//             {
-//                 useNewUrlParser: true,
-//                 useUnifiedTopology: true
-//             }
-//         )
-//         console.log("Mongodb connected")
-//     }catch(err){
-//         console.log("DB error",err)
-//     }
-// }
-// module.exports = connectDB
+const connectDB = async () => {
+    try{
+        await mongoose.connect(
+            CONNECTION_STRING, 
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            }
+        )
+        console.log("Mongodb connected")
+    }catch(err){
+        console.log("DB error",err)
+    }
+}
+module.exports = connectDB
